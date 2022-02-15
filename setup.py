25da15bb9b78984fe2028bdff8b1e48ad8bf2da3
Michael Sonner <>
2022-02-15 17:15:33 +0100

    added optional scipy dependency

--- setup.py
+++ setup.py
@@ -8,4 +8,5 @@ setup(
     author="Michael Sonner",
     author_email="sonnerm@gmail.com",
     description="Toolkit for performing numerical calculations involving free fermion models",
+    extras_require={"sparse linalg":["scipy"]},
 )
