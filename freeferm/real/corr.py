1ea5be363a186c142d82c43d3433d854a3b0e638
Michael Sonner <>
2022-02-15 17:17:44 +0100

    added corr_to_dense

--- freeferm/real/corr.py
+++ freeferm/real/corr.py
@@ -1,11 +1,17 @@
 import numpy as np
+import numpy.linalg as la
 from .. import check_sparse_lmax
-def corr_to_dense(corr):
+from .quad import quad_sb_to_sparse
+def corr_to_dense(corr,sparse=False):
     '''
         Find the dense vector corresponding to the Gaussian state with correlation matrix corr.
     '''
     check_sparse_lmax(corr.shape[0]//2)
-    raise NotImplementedError()
+    if sparse:
+        import scipy.sparse.linalg as sla
+        sla.eigsh(quad_sb_to_sparse(corr),k=1,which="SA")[1][:,0]
+    else:
+        return la.eigh(quad_sb_to_dense(corr))[1][:,0] #for now maybe later change algo to fishman
 def corr_to_mps(corr,nbcutoff=1e-10,chi=None,svd_cutoff=None):
     '''
         Find an MPS
@@ -26,6 +32,11 @@ def dense_to_corr(dense):
     '''
         Calculate the correlation matrix of a dense vector
     '''
-    raise NotImplementedError()
+    L=int(np.log2(dense.shape[0]))
+    corr=np.zeros((2*L,2*L),dtype=complex)
+    for i in range(2*L):
+        for j in range(2*L):
+            corr[i,j]=dense.conj()@(ma(L,i)@(ma(L,j)@dense))
+    return corr-np.eye(2*L) #our convention for the correlation matrix
 def corr_vac(L):
     return np.diag(([1.0j,0]*L)[:-1],1)+np.diag(([-1.0j,0]*L)[:-1],-1)
--- freeferm/real/rot.py
+++ freeferm/real/rot.py
@@ -1,6 +1,12 @@
 import numpy as np
+from .ops import dense_ma
 def rot_dense_to_sb(rot):
-    raise NotImplementedError()
+    L=int(np.log2(rot.shape[0]))
+    ret=np.zeros((2*L,2*L))
+    for i in range(2*L):
+        for j in range(2*L):
+            ret[i,j]=np.trace(dense_ma(L,j)@rot.T.conj()@dense_ma(L,i)@rot).real/2**L
+    return ret
 
 def rot_sb_to_dense(rot):
     raise NotImplementedError()
