c9d4378e45ff5e876fe426600f927ea90f3eb9dd
Michael Sonner <sonnerm@gmail.com>
2022-02-18 16:00:53 +0100

    added projective test for rotation

--- tests/real/test_rot.py
+++ tests/real/test_rot.py
@@ -23,6 +23,30 @@ def test_rot_sb_dense_unitary(seed_rng):
     assert u_sb==pytest.approx(rot_dense_to_sb(u_dense))
     u_dense=rot_sb_to_dense(-u_sb)
     assert -u_sb==pytest.approx(rot_dense_to_sb(u_dense))
+
+def test_rot_sb_dense_unitary_projective(seed_rng):
+    #most of the time it is enough if the rot_sb_to_dense+inverse are correct in the projective sense
+    L=4
+    ham_sb=np.random.random(size=(2*L,2*L))
+    ham_sb=ham_sb+ham_sb.T
+    u_sb=la.eigh(ham_sb)[1]#this is a orthogonal now
+    u_dense=rot_sb_to_dense(u_sb)
+    assert u_dense.shape[0]==2**L
+    assert u_dense.T.conj()@u_dense==pytest.approx(np.eye(2**L))
+    assert u_dense@u_dense.T.conj()==pytest.approx(np.eye(2**L))
+    u_sbd=rot_dense_to_sb(u_dense)
+    assert (u_sb==pytest.approx(u_sbd)) or (-u_sb==pytest.approx(u_sbd))
+    #Testing all 4 sectors of O(2n)
+    u_dense=rot_sb_to_dense(-u_sb)
+    u_sbd=rot_dense_to_sb(u_dense)
+    assert (u_sb==pytest.approx(u_sbd)) or (-u_sb==pytest.approx(u_sbd))
+    u_sb=u_sb@np.diag([-1]+[1]*(2*L-1))
+    u_dense=rot_sb_to_dense(u_sb)
+    u_sbd=rot_dense_to_sb(u_dense)
+    assert (u_sb==pytest.approx(u_sbd)) or (-u_sb==pytest.approx(u_sbd))
+    u_dense=rot_sb_to_dense(-u_sb)
+    u_sbd=rot_dense_to_sb(u_dense)
+    assert (u_sb==pytest.approx(u_sbd)) or (-u_sb==pytest.approx(u_sbd))
 @pytest.mark.xfail(reason="Sparse not implemented yet")
 def test_rot_sb_sparse_unitary(seed_rng):
     L=4
