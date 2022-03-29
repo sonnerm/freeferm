a7a953d7097f4aab6366560a89a9546f6eb8961e
Michael Sonner <sonnerm@gmail.com>
2022-03-29 11:19:10 +0200

    mps_to_corr works now

--- freeferm/real/corr.py
+++ freeferm/real/corr.py
@@ -1,10 +1,11 @@
 import numpy as np
+import math
 import numpy.linalg as la
 from .. import check_sparse_lmax,apply_circuit_to_mps,mps_vac
 from .quad import quad_sb_to_sparse,quad_sb_to_dense
 from .ops import dense_ma
 from .circuit import corr_to_circuit
-from ttarray.raw import recluster,right_canonicalize
+from ttarray.raw import recluster,right_canonicalize,left_canonicalize
 def corr_to_dense(corr,sparse=False):
     '''
         Find the dense vector corresponding to the Gaussian state with correlation matrix corr.
@@ -15,7 +16,7 @@ def corr_to_dense(corr,sparse=False):
         sla.eigsh(quad_sb_to_sparse(corr),k=1,which="SA")[1][:,0]
     else:
         return la.eigh(quad_sb_to_dense(corr))[1][:,0]
-def corr_to_mps(corr,cluster=None,nbcutoff=1e-10,chi=None,svd_cutoff=None):
+def corr_to_mps(corr,cluster=None,nbcutoff=1e-10,chi=None,svd_cutoff=0.0):
     '''
         Find an MPS
     '''
@@ -23,35 +24,47 @@ def corr_to_mps(corr,cluster=None,nbcutoff=1e-10,chi=None,svd_cutoff=None):
     circ=corr_to_circuit(corr,nbcutoff)
     return apply_circuit_to_mps(mps_vac(L,cluster=cluster),circ,chi,svd_cutoff)
 
-def mps_to_corr(mps):
+def mps_to_corr(mps,normalize=False):
     '''
         Calculate the correlation matrix of an MPS
     '''
     #clustering ..., i think i'll just recluster to 2,2,2, ...
     counter=0
     for m in mps:
-        counter+=int(math.log2(m.shape[0]))
+        counter+=int(math.log2(m.shape[1]))
     mps=recluster(mps,((2,),)*counter)
     right_canonicalize(mps)
     ret=np.zeros((len(mps)*2,len(mps)*2),dtype=complex)
     SX=np.array([[0,1],[1,0]])
-    SY=np.array([[0,1j],[0,-1j]])
+    SY=np.array([[0,1j],[-1j,0]])
     SZ=np.array([[1,0],[0,-1]])
     ID=np.array([[1,0],[0,1]])
-    bd=np.array([1.0])
+    bd=np.array([[1.0]])
+    norm=1.0
     for i in range(len(mps)):
-        bdsx=np.einsum("ab,bde,afg,df->eg",bd,mps[i],mps[i].conj(),SX)
-        bdsy=np.einsum("ab,bde,afg,df->eg",bd,mps[i],mps[i].conj(),SY)
-        for j in range(i,len(mps)):
-            ret[2*i,2*j]=np.einsum("ab,bde,afg,df,eg",bdsx,mps[j],mps[j].conj(),SX,np.eye(mps[j].shape[-1]))
-            ret[2*i,2*j+1]=np.einsum("ab,bde,afg,df,eg",bdsx,mps[j],mps[j].conj(),SY,np.eye(mps[j].shape[-1]))
-            ret[2*i+1,2*j]=np.einsum("ab,bde,afg,df,eg",bdsy,mps[j],mps[j].conj(),SX,np.eye(mps[j].shape[-1]))
-            ret[2*i+1,2*j+1]=np.einsum("ab,bde,afg,df,eg",bdsy,mps[j],mps[j].conj(),SY,np.eye(mps[j].shape[-1]))
-            bdsx=np.einsum("ab,bde,afg,df->eg",bdsx,mps[i],mps[i].conj(),SZ)
-            bdsy=np.einsum("ab,bde,afg,df->eg",bdsy,mps[i],mps[i].conj(),SZ)
-        bd=np.einsum("ab,bde,afg,df->eg",bd,mps[i],mps[i].conj(),ID)
+        bdi=np.tensordot(bd,mps[i],axes=((0,),(0,)))
+        bdi=np.tensordot(bdi,mps[i].conj(),axes=((0,),(0,)))
+
+        bdsx=np.tensordot(bdi,SX,axes=((0,2),(0,1)))
+        bdsy=np.tensordot(bdi,SY,axes=((0,2),(0,1)))
+        if normalize:
+            norm=np.trace(bd)
+        ret[2*i,2*i]=0.0 # convention
+        ret[2*i+1,2*i]=1.0j*np.trace(np.tensordot(bdi,SZ,axes=((0,2),(0,1))))
+        for j in range(i+1,len(mps)):
+            bdix=np.tensordot(bdsx,mps[j],axes=((0,),(0,)))
+            bdix=np.tensordot(bdix,mps[j].conj(),axes=((0,),(0,)))
+            bdiy=np.tensordot(bdsy,mps[j],axes=((0,),(0,)))
+            bdiy=np.tensordot(bdiy,mps[j].conj(),axes=((0,),(0,)))
+            ret[2*i,2*j]=1.0j*np.trace(np.tensordot(bdiy,SX,axes=((0,2),(0,1))))
+            ret[2*i,2*j+1]=1.0j*np.trace(np.tensordot(bdiy,SY,axes=((0,2),(0,1))))
+            ret[2*i+1,2*j]=-1.0j*np.trace(np.tensordot(bdix,SX,axes=((0,2),(0,1))))
+            ret[2*i+1,2*j+1]=-1.0j*np.trace(np.tensordot(bdix,SY,axes=((0,2),(0,1))))
+            bdsx=np.tensordot(bdix,SZ,axes=((0,2),(0,1)))
+            bdsy=np.tensordot(bdiy,SZ,axes=((0,2),(0,1)))
+        bd=np.tensordot(bdi,ID,axes=((0,2),(0,1)))
     ret-=ret.T
-    return ret
+    return -ret/2/norm
 
 def dense_to_corr(dense):
     '''
--- tests/real/test_corr.py
+++ tests/real/test_corr.py
@@ -2,7 +2,7 @@ import pytest
 import numpy as np
 import numpy.linalg as la
 from freeferm import apply_circuit_to_dense,apply_circuit_to_mps,mps_vac,is_canonical
-from ttarray.raw import ttslice_to_dense
+from ttarray.raw import ttslice_to_dense,dense_to_ttslice
 from freeferm.real import corr_vac,corr_full,dense_vac,dense_full
 from freeferm.real import corr_to_dense,dense_to_corr,mps_to_corr,corr_to_mps,corr_to_circuit
 from freeferm.real import quad_sb_to_dense
@@ -29,7 +29,14 @@ def test_corr_dense(seed_rng):
     eve=corr_to_dense(corr)
     assert np.abs(eve.T.conj()@phi)==pytest.approx(1.0)
 
-def test_corr_mps_short(seed_rng):
+def test_corr_to_mps(seed_rng):
+    L=6
+    phi=np.random.random(size=(2**L,))+1.0j*np.random.random(size=(2**L,))
+    ttphi=dense_to_ttslice(phi[None,:,None],((2,),)*L)
+    dcorr=dense_to_corr(phi)
+    dcorr=dcorr-np.diag(np.diag(dcorr))
+    assert mps_to_corr(ttphi)==pytest.approx(dcorr)
+def test_scorr_mps_short(seed_rng):
     L=6
     ham=1.0j*np.random.random(size=(2*L,2*L))
     ham=ham.T.conj()+ham
@@ -47,7 +54,7 @@ def test_corr_mps_short(seed_rng):
     # assert np.abs(densi.T.conj()@phi)==pytest.approx(1.0)
     eve=ttslice_to_dense(mpsi)[0,:,0]
     assert np.abs(eve.T.conj()@phi)==pytest.approx(1.0)
-
+    assert mps_to_corr(mpsi)==pytest.approx(corr)
 def test_corr_mps_short_double(seed_rng):
     L=6
     ham=1.0j*np.random.random(size=(2*L,2*L))
@@ -57,15 +64,16 @@ def test_corr_mps_short_double(seed_rng):
     corr=dense_to_corr(phi)
     mpsi=corr_to_mps(corr,cluster=((4,),)*(L//2))
     eve=ttslice_to_dense(mpsi)[0,:,0]
-    # assert is_canonical(mpsi)
     assert np.abs(eve.T.conj()@phi)==pytest.approx(1.0)
-@pytest.mark.skip
+    assert mps_to_corr(mpsi)==pytest.approx(corr)
+# @pytest.mark.skip
 def test_corr_mps_long(seed_rng):
     L=20
     corr=np.random.random(size=(2*L,2*L))
     corr=corr.T.conj()+corr
     rot=la.eigh(corr)[1]
     corr=rot.T@corr_vac(L)@rot
-    mps=corr_to_mps(corr,chi=128)
-    print([m.shape for m in mps])
-    assert mps_to_corr(mps)==pytest.approx(corr)
+    mps=corr_to_mps(corr,chi=256,svd_cutoff=0.0)
+    res=np.abs((mps_to_corr(mps)-corr))
+    # print(res.ravel()[np.argmax(res)],corr.ravel()[np.argmax(res)])
+    assert mps_to_corr(mps,normalize=True)==pytest.approx(corr,rel=5e-1,abs=1e-1)
