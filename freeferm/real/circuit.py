25dfe24f43d236c08df13180d8fd18a727cc3b60
Michael Sonner <sonnerm@gmail.com>
2022-10-17 16:09:06 +0200

    added support for flamp

--- freeferm/real/circuit.py
+++ freeferm/real/circuit.py
@@ -57,6 +57,29 @@ def _mp_find_sb_gate(target,mp):
         mat[2,:]=mat[3,:]
         mat[3,:]=h
     return mp.matrix(mat)
+
+def _flamp_find_sb_gate(target):
+    #start with a random matrix, set first row to target.real, second to target.imag
+    #run Gram schmidt
+    import flamp
+    mat=np.random.random(size=(4,4))
+    mat=flamp.to_mp(mat)
+    tr=np.array([t.real for t in target])
+    ti=np.array([t.imag for t in target])
+    mat[0]=tr
+    mat[0]/=flamp.sqrt(mat[0]@mat[0])
+    mat[1]=ti
+    mat[1]-=(mat[0]@mat[1])*mat[0]
+    mat[1]/=flamp.sqrt(mat[1]@mat[1])
+    mat[2]-=(mat[0]@mat[2])*mat[0]+(mat[1]@mat[2])*mat[1]
+    mat[2]/=flamp.sqrt(mat[2]@mat[2])
+    mat[3]-=(mat[0]@mat[3])*mat[0]+(mat[1]@mat[3])*mat[1]+(mat[2]@mat[3])*mat[2]
+    mat[3]/=flamp.sqrt(mat[3]@mat[3])
+    if flamp.det(mat)<0:
+        h=mat[2].copy()
+        mat[2]=mat[3]
+        mat[3]=h
+    return mat
 def _mp_to_np(mat):
     return np.array([[np.float64(x) for x in y] for y in mat.tolist()])
 def mp_corr_to_circuit(corr,nbcutoff=1e-10,prec=200):
@@ -93,6 +116,41 @@ def mp_corr_to_circuit(corr,nbcutoff=1e-10,prec=200):
                     vs[k]=(i,mp.diag([1,1,1,-1])@r)
                     break
         return [(v[0],rot_sb_to_dense(_mp_to_np(v[1])).T.conj(),True if mp.det(v[1])<0 else False,_mp_to_np(v[1].T)) for v in vs[::-1]]
+def flamp_corr_to_circuit(corr,nbcutoff=1e-10,prec=200):
+    '''
+        Find a quantum circuit which transforms the vacuum state into the
+        gaussian state with correlation matrix corr using a modified version of
+        the algorithm described by Fishman and White Phys. Rev. B 92, 075132.
+    '''
+    import flamp
+    import gmpy2
+    flamp.set_precision(prec)
+    nbcutoff=gmpy2.mpfr(nbcutoff)
+    ccorr=flamp.to_mp(corr)
+    # ccorr=corr+0.5*np.eye(corr.shape[0])
+    L=corr.shape[0]//2
+    vs=[]
+    for l in range(0,2*L,2):
+        for b in range(2,2*L-l+1,2):
+            sub=ccorr[l:b+l,l:b+l]
+            ev,evv=flamp.eigh(sub)
+            if max(ev)>gmpy2.mpfr(0.5)-nbcutoff:
+                target=evv[:,b-1]
+                break
+            if b==2*L-l:
+                target=evv[:,b-1]
+                import warnings
+                warnings.warn("nbcutoff not reached %s"%str(0.5-max(ev)))
+        for i in range(b-4,-1,-2):
+            vs.append(((i+l)//2,_flamp_find_sb_gate(target[i:i+4])))
+            _apply_rot_to_vec(target,i,vs[-1][1])
+            _apply_rot_to_corr(ccorr,(i+l)//2,vs[-1][1])
+    if ccorr[2*L-2,2*L-1].imag>0:
+        for k,(i,r) in list(enumerate(vs))[::-1]:
+            if i==L-2:
+                vs[k]=(i,flamp.to_mp(np.diag([1,1,1,-1]))@r)
+                break
+    return [(v[0],rot_sb_to_dense(np.array(v[1],dtype=np.float64)).T.conj(),True if flamp.det(v[1])<0 else False,np.array(v[1].T,dtype=np.float64)) for v in vs[::-1]]
     
 def corr_to_circuit(corr,nbcutoff=1e-10):
     '''
